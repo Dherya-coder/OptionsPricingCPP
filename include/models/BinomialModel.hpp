1c65bcea4fd48da1059df660bd210cd40dfe61c5
Dherya-coder <119153807+Dherya-coder@users.noreply.github.com>
2025-10-22 17:57:58 +0530

    Update BinomialModel.hpp

--- include/models/BinomialModel.hpp
+++ include/models/BinomialModel.hpp
@@ -7,7 +7,15 @@
 class BinomialModel : public OptionPricingModel {
 public:
     BinomialModel();
+
+    // Calculates the option price using the binomial tree method
     virtual double calculatePrice(const Option& option) const override;
+
+    // 🔹 NEW: Setter to control number of steps dynamically
+    void setSteps(int n) { numSteps_ = n; }
+
+private:
+    int numSteps_ = 100; // Default number of steps for the binomial tree
 };
 
 #endif // BINOMIAL_MODEL_HPP
